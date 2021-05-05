2404d679abb5014e6d8e769cdd2987bf00a565fa
Tyler <Tyler.Pilkington@mscoding.org>
2021-05-04 23:02:55 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Excel_Spliter {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.openToolStripMenuItem.Text = "open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(333, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Split file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Menu;
+            this.progressBar1.Location = new System.Drawing.Point(279, 292);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(215, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 172);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(229, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(342, 321);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Excel_Spliter {
+    public partial class Form1 : Form {
+        private string filepath;
+        private string[] lines = new string[500];
+        public Form1() {
+            InitializeComponent();
+            progressBar1.Minimum = 1;
+            progressBar1.Value = 1;
+            progressBar1.Step = 1;
+        }
+
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                filepath = (openFileDialog1.FileName);
+                richTextBox1.Text = "File loaded";
+                button1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                string name = saveFileDialog1.FileName;
+                var excelApp = new Excel.Application();
+                
+                
+
+                excelApp.Workbooks.Open(filepath);
+                
+
+                Excel._Worksheet workSheet = (Excel.Worksheet)excelApp.ActiveSheet;
+                
+
+                int row = 1;
+                int copyrow = 0;
+                bool isDone = false;
+                int filecount = 0;
+
+                progressBar1.Maximum = workSheet.UsedRange.Rows.Count;
+
+                while (isDone == false) {
+                    var excelAppCopy = new Excel.Application();
+                    excelAppCopy.Workbooks.Add();
+                    Excel._Worksheet workSheetcopy = (Excel.Worksheet)excelAppCopy.ActiveSheet;
+                    for (int i = 0; i < 500;) {
+                        if (workSheet.Cells[row, "D"] == null) workSheet.Cells[row, "4"] = "no";
+                        if (workSheet.Cells[row, "D"].value == "yes") {
+                            copyrow++;
+                            i++;
+                            if (workSheet.Cells[row + 1, "A"].value == null) {
+                                i = 500;
+                                isDone = true;
+                            }
+                            workSheetcopy.Cells[copyrow, "A"] = workSheet.Cells[row, "A"];
+                            workSheetcopy.Cells[copyrow, "B"] = workSheet.Cells[row, "B"];
+                            workSheetcopy.Cells[copyrow, "C"] = workSheet.Cells[row, "C"];
+                        }
+                        row++;
+                        textBox1.Text = string.Format("{0},{1}",row,workSheet.UsedRange.Rows.Count);
+                        progressBar1.PerformStep();
+
+                    }
+                    copyrow = 0;
+                    filecount++;
+                    if (filecount >= 11) {
+                       name = name.Remove(name.Length - 2,2);
+                    }else if (filecount > 1){
+                        name = name.Remove(name.Length - 1,1);
+                    }
+                    name = string.Format(name + filecount);
+                    workSheetcopy.SaveAs(name);
+                    excelAppCopy.Quit();
+                }
+                excelApp.Quit();
+            }
+            
+
+
+
+
+
+        }
+
+       
+    }
+}
